a3c7c3f3698749acaf56d71552a4faaa4166d5b3
Brian Kemper <Kemper10@gmail.com>
2023-06-15 09:39:14 -0400

    tweaks for hackathon

--- getSignedURL/app.js
+++ getSignedURL/app.js
@@ -24,31 +24,39 @@ const URL_EXPIRATION_SECONDS = 300
 
 // Main Lambda entry point
 exports.handler = async (event) => {
-  return await getUploadURL(event)
-}
+  return await getUploadURLs(event);
+};
 
-const getUploadURL = async function(event) {
-  const randomID = parseInt(Math.random() * 10000000)
-  const Key = `${randomID}.jpg`
+const getUploadURLs = async function (event) {
+  const { documents } = JSON.parse(event.body);
+  const presignedDocuments = [];
 
-  // Get signed URL from S3
-  const s3Params = {
-    Bucket: process.env.UploadBucket,
-    Key,
-    Expires: URL_EXPIRATION_SECONDS,
-    ContentType: 'image/jpeg',
+  for (let i = 0; i < documents.length; i++) {
+    const Key = `VLTesting/${documents[i].filename}`;
+    const s3Params = {
+      Bucket: process.env.UploadBucket,
+      Key,
+      Expires: URL_EXPIRATION_SECONDS,
+      ContentType: documents[i].filetype,
 
-    // This ACL makes the uploaded object publicly readable. You must also uncomment
-    // the extra permission for the Lambda function in the SAM template.
+      // This ACL makes the uploaded object publicly readable. You must also uncomment
+      // the extra permission for the Lambda function in the SAM template.
 
-    // ACL: 'public-read'
+      // ACL: 'public-read'
+    };
+
+    console.log("Params: ", s3Params);
+    const presignedUrl = await s3.getSignedUrlPromise("putObject", s3Params);
+
+    presignedDocuments.push({
+      id: Key,
+      filename: documents[i].filename,
+      filetype: documents[i].filetype,
+      presignedUrl,
+    });
   }
 
-  console.log('Params: ', s3Params)
-  const uploadURL = await s3.getSignedUrlPromise('putObject', s3Params)
-
   return JSON.stringify({
-    uploadURL: uploadURL,
-    Key
-  })
-}
+    presignedDocuments,
+  });
+};
